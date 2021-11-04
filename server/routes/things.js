945024ac675331d9f32bf567ab4344aff655c786
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-04 23:54:11 +0100

    Added post, not working

--- server/routes/things.js
+++ server/routes/things.js
@@ -8,16 +8,33 @@ router.get("/", async (req, res) => {
   res.json(things);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
-  const thing = await Thing.findById(id);
-  res.json(thing);
+  try {
+    const thing = await Thing.findById(id);
+    if (thing) {
+      res.json(thing);
+    } else {
+      const error = new Error("Thing not found");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 404;
+    next(error);
+  }
 });
 
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedThing = await Thing.findByIdAndDelete(id);
-  res.json({ deleted: deletedThing });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedThing = await Thing.findByIdAndDelete(id);
+    res.json({ deleted: deletedThing });
+  } catch (error) {
+    error.code = 400;
+    error.message = "Error al borrar";
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
